303f3f2fc16e99712c0ceb5ec2f1830fde8e6bd3
Lars Beemster <biemster@users.noreply.github.com>
2026-03-04 16:46:29 +0100

    wait for TX done in Idle mode to save power

--- pingpong.c
+++ pingpong.c
@@ -165,7 +165,11 @@ void isler_process_rx() {
 			iSLERLinkConfig(uuid, ISLER_CHANNEL, ISLER_PHY_MODE, payload, /*auto_mode*/0);
 		}
 		iSLERLinkTX();
-		for( int timeout = Ticks_from_Ms(5); !tx_done && timeout >= 0; timeout-- );
+		while(!tx_done) {
+			NVIC->SCTLR &= ~(1 << 2); // don't deep sleep
+			NVIC->SCTLR &= ~(1 << 3); // wfi
+			__WFI();
+		}
 		iSLERLinkRX();
 	}
 	else if(gs_iSLERLink.is_open) {
